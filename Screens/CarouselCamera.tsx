2c57e5a101436d0e00b948864526ce133d9950ce
Araz Gadirov <araz.gadirov@gamil.com>
2022-05-27 18:39:44 +0400

    Carousel Camera/ add some changes

--- Screens/CarouselCamera.tsx
+++ Screens/CarouselCamera.tsx
@@ -1,36 +1,60 @@
-import React, { useRef, useState } from 'react'
-import { StyleSheet, View, Text, Pressable, FlatList } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
+import { StyleSheet, View, Text, Pressable, FlatList, Dimensions } from 'react-native'
 import { RNCamera } from 'react-native-camera';
 import { CloseModalHeader } from '../Components/CloseModalHeader';
 
 export const CarouselCamera = () => {
+    const { width, height } = Dimensions.get('screen')
     const [onFocus, setOnFocus] = useState(false)
+    const [isTakenPhoto, setIsTakenPhoto] = useState(false)
+    const [index, setIndex] = useState(0)
+    const [viewPosition, setViewPosition] = useState(0)
     const cameraRef = useRef<any>();
-    let flatlistRef = useRef<any>()
+    let flatlistRef = useRef<FlatList>(null)
     const DATA = [
         { id: "1", title: "A", },
-        { id: "2",  title: "B", },
-        { id: "3",  title: "C", },
+        { id: "2", title: "B", },
+        { id: "3", title: "C", },
         { id: "4", title: "D", },
         { id: "5", title: "E", },
         { id: "6", title: "F", },
         { id: "7", title: "G", },
-      ];
-    const RenderItem = ({item} : any) => {
-        console.log
-          return    (
-                <Pressable  onPress={takePicture} style={{...styles.takePicture, backgroundColor: 'black'}}>
-                    <Text>{item.id}</Text>
-                </Pressable>
-            );
-    }
-    
+        { id: "8", title: "A", },
+        { id: "9", title: "B", },
+        { id: "10", title: "C", },
+        { id: "11", title: "D", },
+        { id: "12", title: "E", },
+        { id: "13", title: "F", },
+        { id: "14", title: "G", },
+    ];
+    // const RenderItem = ({ item }: any) => {
+    //     return (
+    //         <Pressable onPress={takePicture} style={{ ...styles.takePicture, backgroundColor:'black' }}>
+    //             <Text>{item.id}</Text>
+    //         </Pressable>
+    //     );
+    // }
+
     const takePicture = async () => {
         if (cameraRef) {
             const options = { quality: 0.5, base64: true };
             const data = await cameraRef.current.takePictureAsync(options)
         }
+        setIsTakenPhoto(true);
     };
+
+    const reload = () => (
+        setIsTakenPhoto(false)
+    )
+
+useEffect(()=> {
+    flatlistRef.current?.scrollToIndex({
+        index,
+        animated: true,
+        viewOffset: viewPosition === 0.5 || viewPosition === 1 ? 0 : 10,
+        viewPosition: 0.5
+    })
+}, [index, viewPosition])
 
     return (
         <View style={styles.container}>
@@ -48,25 +72,37 @@ export const CarouselCamera = () => {
             >
                 <CloseModalHeader style={styles.headerCarouselCam} />
                 <View style={styles.CarouslCamFooter}>
-                    <Pressable style={styles.reload}>
-                        <Text> ph </Text>
+                    <Pressable onPress={() => reload()} style={styles.reload}>
+                        <Text> {isTakenPhoto ? 're' : 'ph'}  </Text>
                     </Pressable>
                     <Text style={styles.footerText}>RIGHT FRONT DOOR</Text>
                 </View>
 
             </RNCamera>
-            <FlatList
-                data={DATA}
-                renderItem={({item})=> {
-                    return <RenderItem item={item}/>
-                }}
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                ref={(ref) => {
-                    flatlistRef.current = ref
-                }}
-                style={styles.carousel}
-            />
+            {isTakenPhoto ?
+                <View style={{ ...styles.takePicture, position: 'absolute', bottom: '15%', backgroundColor: '#3785F7' }}>
+                    <Text>✓</Text>
+                </View>
+                :
+                <FlatList
+                    data={DATA}
+                    renderItem={({ item, index: fIndex }) => {
+                        return (
+                            <Pressable onPress={()=> {
+                                setIndex(fIndex)
+                            }} style={{ ...styles.takePicture, backgroundColor: fIndex === index?  'grey' : 'black' }}>
+                                <Text>{item.id}</Text>
+                            </Pressable>
+                        )
+                    }}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    ref={flatlistRef}
+                    style={styles.carousel}
+                    initialScrollIndex={index}
+
+                />
+            }
         </View>
     )
 }
